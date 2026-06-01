3a3410aa48836f55462156da05407b615c2a0aae
JosephM957 <josephmbuisness5@gmail.com>
2026-06-01 17:07:11 -0500

    Add completed C# challenge 1

--- C#/Challenge1/Program.cs
+++ C#/Challenge1/Program.cs
@@ -1,18 +1,28 @@
-﻿//CURRENTLY IN PROGRESS NOT COMPLETED YET
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 namespace Challenge1 {
     internal class Challenge1
     {
         static void Main(string[] args)
         {
-
-            Dictionary<string, int> result = GetTopWords("This is me testing. Punctation!,.", 5);
+            Dictionary<string, int> result;
+            Console.WriteLine("Enter what text you would like to create tags for");
+            string text = Console.ReadLine();
+            Console.WriteLine("Enter how many tags you would like this article to have");
+            int items = Convert.ToInt32(Console.ReadLine());
+            result = GetTopWords(text, items);
+            foreach (var pair in result)
+            {
+                Console.WriteLine($"Key: \"{pair.Key}\", Value: {pair.Value}");
+            }
         }
         public static Dictionary<string, int> GetTopWords(string text, int topN)
         {
             string[] words;
             Dictionary<string, int> wordAppearances = [];
+            Dictionary<string, int> finalwords = [];
+            int[] values = [];
             string[] punctuation = { "!", ",", ".", "?" };
+            Dictionary<string, int> results = [];
             foreach (string punct in punctuation)
             {
                 text = text.Replace(punct, string.Empty);
@@ -29,11 +39,26 @@ namespace Challenge1 {
                     wordAppearances.Add(word, 1);
                 }
             }
-            wordAppearnces = wordAppearnces.OrderByDescending(wordAppearances);
-            foreach (var word in wordAppearances)
+            foreach (KeyValuePair<string, int> word in wordAppearances.OrderByDescending(key => key.Value).ThenBy(word => word.Key))
             {
+                finalwords.Add(word.Key, word.Value);
             }
-            return wordAppearances;
+            if (finalwords.Count != 0)
+            {
+                int instances = finalwords.Count;
+                for (int i = 0; i < instances && i < topN; i++)
+                {
+                    var pair = finalwords.ElementAt(i);
+                    results.Add(pair.Key, pair.Value);
+
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Count = {results.Count}");
+                Console.WriteLine("Test Case 3: N is larger than unique words");
+            }
+            return results;
         }
     }
 }
